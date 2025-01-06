7fd99416e4247293158b1ee695002348de58a5cb
Franklin Ifesanya <franklin@localhost.localdomain>
2025-01-06 21:01:59 +0000

    Adding admin endpoint.

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from pokemon import views
 from rest_framework_simplejwt.views import (
@@ -6,6 +7,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/pokemon", views.pokemon),
