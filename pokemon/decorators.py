2755d140d14d6f662e935522bb5fe86c9d88fed0
Franklin Ifesanya <franklin@localhost.localdomain>
2025-01-06 21:15:45 +0000

    Improving decorator.

--- pokemon/decorators.py
+++ pokemon/decorators.py
@@ -2,8 +2,11 @@ from django.http import HttpResponseForbidden
 from .models import User, UserRole
 
 def get_user_role(user):
-    user_role= UserRole.objects.get(user=User.objects.get(username=user))
-    return user_role.role
+    try:
+        user_role = UserRole.objects.get(user=User.objects.get(username=user))
+        return user_role.role
+    except:
+        return False
 
 def has_permission(perm_name):
     def decorator(view_func):
